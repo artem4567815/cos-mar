241b71d3bec1560fcb00b5a9599cc0a7688bd31c
Sch1561-it-14 <artemiy_08@icloud.com>
2023-04-13 17:19:02 +0300

    controoler32

--- main2.py
+++ main2.py
@@ -907,13 +907,13 @@ class MyControoler(Controller):
     def on_R2_release(self):
         Move_two_bul(None)
     def on_L3_down(self, value):
-        global is_playing
+        global is_playing, time2
         if is_playing:
             x, y = c.coords(game_objects["aim2"])
             if y < 600:
                 c.move(game_objects["aim2"], 0, vy)
                 c.move(game_objects["aim"], 0, vy)
-        tk.after(1000, move_aim_down)
+        tk.after(time2, move_aim_down)
 
 
 controller = MyControoler(interface="/dev/input/js0", connecting_using_ds4drv=False)
