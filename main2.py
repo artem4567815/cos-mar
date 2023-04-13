db0875a2055104aab2ba86af0fbc20fd21cb525c
Sch1561-it-14 <artemiy_08@icloud.com>
2023-04-13 15:24:23 +0300

    controoler4

--- main2.py
+++ main2.py
@@ -12,6 +12,22 @@ from operator import itemgetter
 # import os
 # import sys
 # os.chdir(sys._MEIPASS)
+from pyPS4Controller.controller import Controller
+
+class MyControoler(Controller):
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+    def on_x_press(self):
+        spawn_bullet()
+    def on_x_release(self):
+        print("Goodbye")
+
+
+controller = MyControoler(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen(timeout=60)
+controller.on_x_press()
+controller.on_x_release()
+
 
 tk = Tk()
 style = ttk.Style()
--- testing_creat_web-server.py
+++ testing_creat_web-server.py
@@ -1,10 +1,11 @@
 from pyPS4Controller.controller import Controller
+from main2 import spawn_bullet
 
 class MyControoler(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
     def on_x_press(self):
-        print("helloWorld")
+        spawn_bullet()
     def on_x_release(self):
         print("Goodbye")
 
