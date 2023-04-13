240ac22171837d30d8b03ffb16dde7fadd1744dc
Sch1561-it-14 <artemiy_08@icloud.com>
2023-04-13 16:07:19 +0300

    controoler17

--- main2.py
+++ main2.py
@@ -918,6 +918,6 @@ class MyControoler(Controller):
 
 
 controller = MyControoler(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen(timeout=60)
+#controller.listen(timeout=60)
 
 mainloop()
