2d0f1978339a5a9083b9d234b1605c5414611c3d
Sch1561-it-14 <artemiy_08@icloud.com>
2023-04-13 15:10:51 +0300

    controoler

---
+++ testing_creat_web-server.py
@@ -0,0 +1,13 @@
+from pyPS4Controller.controller import Controller
+
+class MyControoler(Controller):
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+        def on_x_press(self):
+            print("helloWorld")
+        def on_x_release(self):
+            print("Goodbye")
+
+
+controller = MyControoler(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen(timeout=60)
