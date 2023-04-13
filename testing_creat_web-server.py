c51ac727ade55e3243225821ac7ed17f8701c3f9
Sch1561-it-14 <artemiy_08@icloud.com>
2023-04-13 15:21:37 +0300

    controoler3

--- testing_creat_web-server.py
+++ testing_creat_web-server.py
@@ -3,10 +3,10 @@ from pyPS4Controller.controller import Controller
 class MyControoler(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
-        def on_x_press(self):
-            print("helloWorld")
-        def on_x_release(self):
-            print("Goodbye")
+    def on_x_press(self):
+        print("helloWorld")
+    def on_x_release(self):
+        print("Goodbye")
 
 
 controller = MyControoler(interface="/dev/input/js0", connecting_using_ds4drv=False)
