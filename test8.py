0a5cf2e38316cf2bad7ed9bb3538188a7c096495
Sch1561-it-23 <artemiy_08@icloud.com>
2023-04-21 18:02:40 +0300

    cpommit5

--- test8.py
+++ test8.py
@@ -250,8 +250,8 @@ def menn():
 def draw_menu2():
     global station, leb, leb2, num, btn4, lab, lab2, leb3
     if not is_playing:
-        objects.append(c.create_image(150, 130, image=restart, anchor=NW))
-        objects.append(c.create_image(100, 469, image=tab_lid, anchor=NW))
+        objects.append(c.create_image(100, 80, image=restart, anchor=NW))
+        objects.append(c.create_image(80, 419, image=tab_lid, anchor=NW))
         if flag7:
             station = "menu3"
             lab.destroy()
